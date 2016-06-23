e6182c1b661550ed4deac8bf3dd336e81856dab3
Ed Madrid <edmadrid@gmail.com>
2016-06-23 11:48:36 -0400

    minor wording fixes

--- template-parts/content-editorssingle.php
+++ template-parts/content-editorssingle.php
@@ -15,9 +15,9 @@
 		<?php endif; ?>
 
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-		
+				<span class="byline">Anna McCarthy, Web Editor</span>
 				<span class="posted-on"><?php the_date('F j, Y'); ?></span>
-		
+
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -37,16 +37,16 @@
 				'separator'   => '<span class="screen-reader-text">, </span>',
 			) );
 		?>
-		
-		
+
+
 		<?php the_tags( 'Tagged with: ', ', ', '<br />' ); ?>
-		
+
 		<span class="feed_item_cat"><a href="<?php echo esc_url( home_url( '/' ) ); ?>editorsblog">Back to the Editor's Blog</a></span>
-		
+
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-	
+
 		<?php twentysixteen_entry_meta(); ?>
 		<?php
 			edit_post_link(
